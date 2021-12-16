daaa9d3bc2de3f4b3e244ca06de905b3e8f19449
aimee-annabelle <inezannabelle@gmail.com>
2021-12-16 16:42:28 +0200

    comp freezing

--- src/test/java/rw/ac/rca/termOneExam/service/CityServiceTest.java
+++ src/test/java/rw/ac/rca/termOneExam/service/CityServiceTest.java
@@ -32,15 +32,46 @@ public class CityServiceTest {
     @Test
     public void all_test() {
         when(cityRepository.findAll()).thenReturn(Arrays.asList(new City("Kigali",10), new City("Huye",20)));
-
         assertEquals("Huye", cityService.getAll().get(1).getName());
     }
 
     @Test
+    public void all_test_fail() {
+        when(cityRepositoryMock.findAll())
+                .thenReturn(null);
+        assertEquals(null,cityService.getAll());
+    }
+    @Test
     public void findById_test() {
         when(cityRepository.findById(anyLong())).thenReturn(Optional.of(new City("Kigali",24)));
-
         assertEquals("Kigali", cityService.getById(101).get().getName());
     }
-    
+    @Test
+    public void findById_test_404() {
+        when(cityRepository.findById(100L)).thenReturn(Optional.empty());
+
+        assertEquals(false, cityService.getById(100L).isPresent());
+    }
+
+    @Test
+    public void existsByName_success() {
+        when(cityRepository.existsByName("Kigali")).thenReturn(true);
+        assertEquals(true, cityService.existsByName("Kigali"));
+    }
+
+    @Test
+    public void existsByName_fail() {
+        when(cityRepository.existsByName("Kagali")).thenReturn(false);
+
+        assertEquals(false, cityService.existsByName("Kagali"));
+    }
+
+//    @Test
+//    public void createCity_success() {
+//        CreateCityDTO cityDTO = new CreateCityDTO("Huye",27);
+//        when(cityRepository.save(any(City.class))).thenReturn(new City(cityDTO.getName(),cityDTO.getWeather()));
+//
+//        assertEquals(cityDTO.getName(), cityService.save(cityDTO).getName());
+//        assertEquals(TempratureFahrenheitConversion.tempToFah(cityDTO.getWeather()), cityService.save(cityDTO).getFahrenheit());
+//    }
 }
--- src/test/java/rw/ac/rca/termOneExam/utils/CityUtilTest.java
+++ src/test/java/rw/ac/rca/termOneExam/utils/CityUtilTest.java
@@ -3,6 +3,7 @@ package rw.ac.rca.termOneExam.utils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import rw.ac.rca.termOneExam.domain.City;
 import rw.ac.rca.termOneExam.repository.ICityRepository;
@@ -11,11 +12,13 @@ import java.util.List;
 
 import static org.junit.Assert.assertTrue;
 
+@DataJpaTest
 @RunWith(SpringRunner.class)
 public class CityUtilTest {
 
     @Autowired
     private ICityRepository cityRepository;
+
     @Test
     public void weatherLessThan40DegreesTest_success(){
         List<City> cities = cityRepository.findAll();
